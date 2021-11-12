2f40e4c0c8194549cf68d675eddf8c2e80e8f197
arnu515 <arnu5152@gmail.com>
2021-11-12 20:05:47 +0530

    feat: add frontend

---
+++ client/src/lib/components/Header.svelte
@@ -0,0 +1,19 @@
+<header class="header">
+  <div class="brand">Realworld Chat</div>
+  <a href="https://github.com/arnu515/python-realworld-chat" class="source-code">Source</a>
+</header>
+
+<style lang="postcss">
+  .header {
+    @apply bg-blue-500 text-white px-4 py-2 flex items-center;
+  }
+  .brand {
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+  }
+  .source-code {
+    @apply ml-auto mr-2 px-4 py-2 uppercase;
+    font-size: 14px;
+  }
+</style>
