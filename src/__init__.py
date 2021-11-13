4a5ddc1a2bc82d8b36cb637a2c293bc21c0e8e3f
arnu515 <arnu5152@gmail.com>
2021-11-13 07:46:46 +0530

    feat: add auth router

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
 from .app import app  # noqa: F401
+from .routes import routes
+
+for router in routes:
+    app.include_router(router)
---
+++ src/routes/__init__.py
@@ -0,0 +1,3 @@
+from .auth import router as auth
+
+routes = [auth]
